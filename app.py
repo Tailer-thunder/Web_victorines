8010d3dba4624c63eebe1518b804e86dec0b0357
Tailer-thunder <korobejnikov.d@inbox.ru>
2024-04-05 04:16:56 +0300

    Test quiz

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_text = db.Column(db.String(500), nullable=False)
+    correct_answer = db.Column(db.String(100), nullable=False)
+    wrong_answer1 = db.Column(db.String(100), nullable=False)
+    wrong_answer2 = db.Column(db.String(100), nullable=False)
+    wrong_answer3 = db.Column(db.String(100), nullable=False)
 
 @app.route('/')
 def index():
@@ -72,7 +79,45 @@ def quiz_selection():
     return render_template('quiz_selection.html')
 @app.route('/start_quiz/<quiz_id>', methods=['GET', 'POST'])
 def start_quiz(quiz_id):
-    pass
+    return f"Starting quiz {quiz_id}"
+
+@app.route('/start_quiz/breaking_bad', methods=['GET'])
+def start_breaking_bad_quiz():
+    questions = [
+        {
+            'question': 'What is the main character\'s name in Breaking Bad?',
+            'answers': ['Walter White', 'Jesse Pinkman', 'Saul Goodman', 'Gus Fring'],
+            'correct_answer': 'Walter White'
+        },
+        {
+            'question': 'Who is Jesse Pinkman\'s former chemistry teacher?',
+            'answers': ['Walter White', 'Gus Fring', 'Saul Goodman', 'Mike Ehrmantraut'],
+            'correct_answer': 'Walter White'
+        }
+        # можно ещё вопросов, наподобие этого ещё викторин сделать черезе ссылочку по кнопочке из quiz_selection
+    ]
+    return render_template('quiz.html', questions=questions)
+
+
+@app.route('/submit_quiz_answers', methods=['POST'])
+def submit_quiz_answers():
+    # Получаем ответы пользователя из запроса
+    user_answers = request.form
+
+    # Правильные ответы только на эту викторину к сожалению ((((
+    correct_answers = {
+        "What is the main character's name in Breaking Bad?": 'Walter White',
+        "Who is Jesse Pinkman's former chemistry teacher?": 'Walter White'
+    }
+
+    # Подсчитываем количество правильных ответов
+    num_correct_answers = 0
+    for question, user_answer in user_answers.items():
+        if user_answer == correct_answers.get(question):
+            num_correct_answers += 1
+
+    # Возвращаем результаты на отдельную страницу
+    return render_template('quiz_results.html', num_correct=num_correct_answers, total_questions=len(correct_answers))
 
 if __name__ == '__main__':
     with app.app_context():
